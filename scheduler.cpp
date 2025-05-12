c2550a0f273bfd31a42449d3053ed5cb3836c1c6
Typho Dang <typhod21@vt.edu>
2025-05-11 20:09:14 -0400

    k

--- scheduler.cpp
+++ scheduler.cpp
@@ -75,6 +75,7 @@ void Scheduler::setPriority()
 
 bool Scheduler::runRMDMTest(std::vector<Task> taskSet)
 {
+	setPriority();
     cout << "\nRunning RM/DM schedulability tests...\n";
     double utilization = computeUtilization();
     double bound = taskSet.size() * (pow(2, 1.0 / taskSet.size()) - 1);
@@ -125,7 +126,7 @@ bool Scheduler::runRMDMTest(std::vector<Task> taskSet)
             cout << "Task " << task.id << " is schedulable with response time: " << responseTime << "\n\n";
         }
     }
-
+	
     return schedulable;
 }
 
